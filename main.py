c1331a3223d119dc25e0b462b98808b43a3e0b7b
Anushka Srivastava <119046503+anushk4@users.noreply.github.com>
2024-02-02 00:41:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 if __name__ == "__main__":
+    # Main function
     try:
+        # Input format: 1 2 3 4 5...
         array = list(map(int,input("Enter your list as space-separated values: ").split()))
+        """
+        Displays error: invalid literal for int() with base 10: {str}
+        if non-integer values are entered
+        """
+
+        # Error handling to check length of array
         if len(array) % 10 != 0:
             raise ValueError("Error: List length must be a multiple of 10.")
+        
+        # 1-based indexing
+        # Skipping values at position of multiples of 2 and 3
         result = [item for index, item in enumerate(array) if (index + 1) % 2 != 0 and (index + 1) % 3 != 0]
         print("Output List: ",result)
         
+    # Action when ValueError is encountered
     except ValueError as e:
         print(str(e))
 
+    # Handling miscellaneous errors
     except Exception as e:
         print(f"Unexpected error: {str(e)}")
